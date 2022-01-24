ae650e7f3d8a8d16eb608f19094a88f4403603e0
Erika Donato <erikaadonato@gmail.com>
2022-01-24 20:40:38 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet";
+
+function App() {
+  const [tweets, setTweets] = useState<string[]>([
+    'Tweet 1', 
+    'Tweet 2', 
+    'Tweet 3', 
+    'Tweet 4', 
+  ])
+
+  function createTweet(){
+    setTweets([...tweets, 'Tweet 5'])
+  }
+
+  return (
+    <div>
+      {tweets.map((tweets)=> {
+        return <Tweet text={tweets} />
+      })}
+      <button onClick={createTweet}>Adicionar tweet</button>
+    </div>
+  )
+}
+
+export default App
